aa1e18a4825b8bcfe63937ddf1ca681395827989
Bendik Solheim <hello@bendik.dev>
2020-11-25 23:41:21 +0100

    Invert Y axis in function making walls. Add axis

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -5,7 +5,17 @@ import Controlled from "../components/controlled";
 import Velocity from "../components/velocity";
 import GameScene from "../components/game-scene";
 import Polygon from "../components/polygon";
-import { vec2D } from "../primitives/vec2d";
+import { Vec2D, vec2D } from "../primitives/vec2d";
+import { Line, line } from "../primitives/line";
+import translate from "../math/translate";
+
+const walls = [
+  line(vec2D(-100, 50), vec2D(-100, -50)),
+  line(vec2D(-100, -50), vec2D(-50, -100)),
+  line(vec2D(-50, -100), vec2D(50, -100)),
+  line(vec2D(50, -100), vec2D(100, -50)),
+  line(vec2D(100, -50), vec2D(100, 50)),
+];
 
 export default function initializeGameScene(
   stage: PIXI.Container
@@ -20,11 +30,10 @@ export default function initializeGameScene(
 
   const entities = [
     sceneEntity,
-    player(window.innerWidth / 2, window.innerHeight / 2, scene),
-    // player(12.5, 16, scene),
-    // wall((window.innerWidth / 3) * 2, (window.innerHeight / 3) * 2, scene),
-    wall(100, 100, scene),
-  ];
+    player(0, 0, scene),
+    xAxis(scene),
+    yAxis(scene),
+  ].concat(walls.map((w) => wall(w, scene)));
 
   return [scene, entities];
 }
@@ -48,19 +57,56 @@ export function player(x: number, y: number, stage: PIXI.Container): Entity {
   return player;
 }
 
-export function wall(x: number, y: number, stage: PIXI.Container): Entity {
-  const points = [vec2D(0, 0), vec2D(0, 100)];
+export function wall(line: Line, stage: PIXI.Container): Entity {
+  const localLine = translate(line, vec2D(0, 0));
+  // We invert the y axis to get make positive Y upwards
+  const points = [localLine.from, localLine.to].map((p) => vec2D(p.x, -p.y));
   const path = points.map((p) => [p.x, p.y]).flat();
   const g = new PIXI.Graphics();
   g.name = "wall";
   g.lineStyle(3, 0xcecece, 1);
   g.drawPolygon(path);
-  g.x = x;
-  g.y = y;
+  g.x = line.from.x;
+  // We invert the y axis to get make positive Y upwards
+  g.y = line.from.y * -1;
 
   const wall = new PixiEntity();
   wall.addDisplayObject(g, stage);
   wall.add(new Collidable());
   wall.add(new Polygon(points));
   return wall;
+}
+
+function xAxis(stage: PIXI.Container): Entity {
+  const from = vec2D(0, 0);
+  const to = vec2D(1000, 0);
+  const points = [from, to];
+  const path = points.map((p) => [p.x, p.y]).flat();
+  const g = new PIXI.Graphics();
+  g.name = "axis";
+  g.lineStyle(1, 0xff0000, 0.5);
+  g.drawPolygon(path);
+  g.x = -500;
+  g.y = 0;
+
+  const axis = new PixiEntity();
+  axis.addDisplayObject(g, stage);
+  return axis;
+}
+
+function yAxis(stage: PIXI.Container): Entity {
+  const from = vec2D(0, 0);
+  const to = vec2D(0, 1000);
+  const points = [from, to];
+  const path = points.map((p) => [p.x, p.y]).flat();
+  const g = new PIXI.Graphics();
+  g.name = "axis";
+  g.lineStyle(1, 0xff0000, 0.5);
+  g.drawPolygon(path);
+  g.x = 0;
+  g.y = -500;
+
+  const axis = new PixiEntity();
+  axis.addDisplayObject(g, stage);
+  return axis;
 }
